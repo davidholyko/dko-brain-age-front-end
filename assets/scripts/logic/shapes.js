b2cce030aac1cd789043f2dfe238f31ae2bf5c7b
davidholyko <david.holy.ko@gmail.com>
2019-03-10 20:08:02 -0400

    expand page width

--- assets/scripts/logic/shapes.js
+++ assets/scripts/logic/shapes.js
@@ -1,5 +1,15 @@
 const store = require('../store')
 
+const shapesHTML = {
+  triangle: '<img src="public/images/triangle.png" alt="" id="triangle">',
+  square: '<img src="public/images/square.png" alt="" id="square">',
+  pentagon: '<img src="public/images/pentagon.png" alt="" id="pentagon">',
+  hexagon: '<img src="public/images/hexagon.png" alt="" id="hexagon">',
+  heptagon: '<img src="public/images/heptagon.png" alt="" id="heptagon">',
+  octagon: '<img src="public/images/octagon.png" alt="" id="octagon">',
+  question: '<img src="public/images/question.png" alt="" id="question">'
+}
+
 const generateProblem = () => {
   const shapes = ['triangle', 'square', 'pentagon', 'hexagon', 'heptagon', 'octagon']
   const startIndex = Math.random() * 2 | 0
@@ -11,7 +21,8 @@ const generateProblem = () => {
   const randomIndex = Math.random() * multipleChoiceInput.length | 0
   const answer = multipleChoiceInput[randomIndex]
   multipleChoiceInput[randomIndex] = 'question'
-  const question = `${multipleChoiceInput} Which shape is missing?`
+  const question =
+  `${shapesHTML[multipleChoiceInput[0]]} ${shapesHTML[multipleChoiceInput[1]]} ${shapesHTML[multipleChoiceInput[2]]}    ${shapesHTML[multipleChoiceInput[3]]} ${shapesHTML[multipleChoiceInput[4]]} <h1>Which shape is missing?</h1>`
 
   // randomize MultipleChoice
   for (let i = multipleChoiceInput.length; i > 0; i--) {
