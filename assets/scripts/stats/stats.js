50aed524ebd1ff673ae1716817aa38f51555bbf8
davidholyko <david.holy.ko@gmail.com>
2019-03-13 22:29:21 -0400

    reduce stats font size scaling

--- assets/scripts/stats/stats.js
+++ assets/scripts/stats/stats.js
@@ -16,19 +16,19 @@ const calcAll = () => {
 const calcAverage = () => {
   console.log('calcAverage')
   store.stats.mean = math.mean(store.data)
-  $('#stats').append(`<h1>Global Average Score: ${Math.trunc(store.stats.mean)}`)
+  $('#stats').append(`<h4>Global Average Score: ${Math.trunc(store.stats.mean)}</h4>`)
 }
 
 const calcMedian = () => {
   console.log('calcMedian')
   store.stats.median = math.median(store.data)
-  $('#stats').append(`<h1>Global Median Score: ${store.stats.median}`)
+  $('#stats').append(`<h4>Global Median Score: ${store.stats.median}</h4>`)
 }
 
 const calcMode = () => {
   console.log('calcMode')
   store.stats.mode = math.mode(store.data)
-  $('#stats').append(`<h1>Global Mode Score: ${store.stats.mode}`)
+  $('#stats').append(`<h4>Global Mode Score: ${store.stats.mode}</h4>`)
 }
 
 module.exports = {
