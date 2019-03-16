61d01232eb69fe53ea0ab7a6ae6f1ab1d97dbcc8
davidholyko <david.holy.ko@gmail.com>
2019-03-16 02:51:57 -0400

    add hover suggestion, remove suggestion class from buttons

--- assets/scripts/page/events.js
+++ assets/scripts/page/events.js
@@ -2,46 +2,51 @@
 
 const store = require('../store')
 
+const onToggleGamePage = () => {
+  // console.log('onToggleGamePage')
+  store.currentPage = 'game'
+  $('#stats-page').hide()
+  $('#suggestions-page').hide()
+  $('#game-page').show()
+  $('#toggle-page-title').text('Game Page')
+  if (store.user) {
+    $('#show-all-suggestion-button-container').hide()
+    $('#show-my-suggestion-button-container').hide()
+  }
+}
+
 const onToggleSuggestionsPage = () => {
   // console.log('onToggleSuggestionsPage')
+  store.currentPage = 'suggestion'
+  $('#stats-page').hide()
+  $('#game-page').hide()
+  $('#suggestions-page').show()
+  $('#suggestions-page').css('display', 'flex')
+  $('#toggle-page-title').text('Suggestions Page')
+  if (store.user) {
+    $('#show-all-suggestion-button-container').show()
+    $('#show-my-suggestion-button-container').show()
+  }
+}
 
-  if (store.currentPage === 'game') {
-    store.currentPage = 'suggestion'
-    $('#stats-page').hide()
-    $('#game-page').hide()
-    $('#suggestions-page').show()
-    $('#suggestions-page').css('display', 'flex')
-    $('#toggle-page-button').text('Suggestion Page')
-    if (store.user) {
-      $('#show-all-suggestion-button-container').show()
-      $('#show-my-suggestion-button-container').show()
-    }
-  } else if (store.currentPage === 'suggestion') {
-    store.currentPage = 'stats'
-    $('#suggestions-page').hide()
-    $('#game-page').hide()
-    $('#stats-page').show()
-    $('#toggle-page-button').text('Stats Page')
-    if (store.user) {
-      $('#show-all-suggestion-button-container').hide()
-      $('#show-my-suggestion-button-container').hide()
-    }
-  } else if (store.currentPage === 'stats') {
-    store.currentPage = 'game'
-    $('#stats-page').hide()
-    $('#suggestions-page').hide()
-    $('#game-page').show()
-    $('#toggle-page-button').text('Game Page')
-    if (store.user) {
-      $('#show-all-suggestion-button-container').hide()
-      $('#show-my-suggestion-button-container').hide()
-    }
+const onToggleStatsPage = () => {
+  // console.log('onToggleSuggestionsPage')
+  store.currentPage = 'stats'
+  $('#suggestions-page').hide()
+  $('#game-page').hide()
+  $('#stats-page').show()
+  $('#toggle-page-title').text('Stats Page')
+  if (store.user) {
+    $('#show-all-suggestion-button-container').hide()
+    $('#show-my-suggestion-button-container').hide()
   }
 }
 
 const addHandlers = () => {
   // console.log('addHandlers Page')
-  $('#toggle-page-button').on('click', onToggleSuggestionsPage)
+  $('#toggle-game-page-button').on('click', onToggleGamePage)
+  $('#toggle-suggestions-page-button').on('click', onToggleSuggestionsPage)
+  $('#toggle-stats-page-button').on('click', onToggleStatsPage)
 }
 
 module.exports = {
